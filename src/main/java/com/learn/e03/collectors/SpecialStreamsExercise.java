df12cb94df619ba58bef01bc62600b80dd0d09d4
ubuntu <ubuntu@ip-172-31-83-26.ec2.internal>
2019-11-07 19:05:54 +0000

    SpecialStreams and Collectors Excercise

--- src/main/java/com/learn/e03/collectors/SpecialStreamsExercise.java
+++ src/main/java/com/learn/e03/collectors/SpecialStreamsExercise.java
@@ -1,5 +1,43 @@
 package com.learn.e03.collectors;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class SpecialStreamsExercise {
 
+	
+	static void getSquaresOfThree() {
+		
+		System.out.println(" squares of three are ");
+		
+	LinkedList<Integer> squres = IntStream.rangeClosed(20,50)
+		                          .filter(n->(n%2)!=0)
+		                          .filter(n-> n%3==0)
+		                          .map(n-> n*n)
+		                          .boxed()
+		                          .collect(Collectors.toCollection(LinkedList :: new));
+	
+		squres.forEach(System.out :: println);
+	}
+	
+	static void getMultiplesOfFive() {
+		 System.out.println("multiples of five are ");
+		 int[] result =Stream.iterate(5, n->n+5).limit(20)
+				             .mapToInt(n->n) 
+		                     .toArray();
+		 for(int i : result) {
+			 System.out.println(i);
+		 }
+	}
+	
+	public static void main(String[] args) {
+		
+		getSquaresOfThree();
+		getMultiplesOfFive();
+	}
+	
 }
--- src/main/java/com/learn/e03/collectors/StreamCollectorsExercise.java
+++ src/main/java/com/learn/e03/collectors/StreamCollectorsExercise.java
@@ -1,5 +1,86 @@
 package com.learn.e03.collectors;
 
-public class StreamCollectorsExercise {
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
 
+import com.learn.e03.collectors.Player;
+
+
+public class StreamCollectorsExercise {
+     
+	static List<Player> players = Arrays.asList(new Player[] {
+			new Player("Venkatesh", 300, 100000, 120 , new Country(101 ,"India")),
+			new Player("Anwesh" , 105, 450, 100 , new Country(101 ,"India")),
+			new Player("Jagadesh" , 110, 800, 98 ,new Country(102 ,"USA")),
+			new Player("Bharath" , 8, 500, 80 , new Country(103 ,"London"))
+	});
+	
+	static void getPlayersByCountry() {
+		
+		Map<String,List<String>> playerList = players.stream()
+				                                     .collect(Collectors.groupingBy(p -> p.getCountry().getCountryName(),Collectors.mapping(Player :: getPlayerName, Collectors.toList())));
+	
+	   System.out.println(playerList);
+	
+	}
+	
+	static void getPlayerNamesByCountry() {
+	
+		Map<String,List<String>> playerNames = players.stream().filter(p->p.getMatchesPlayed()>100)
+                .collect(Collectors.groupingBy(p -> p.getCountry().getCountryName(),Collectors.mapping(Player :: getPlayerName, Collectors.toList())));
+
+		System.out.println(playerNames);
+		
+	}
+	
+	static void getTotalPlayersByCountry() {
+		
+	Map<String, Long> totalPlayers = 
+			players.stream().collect(
+					Collectors.groupingBy(
+							(p -> p.getCountry().getCountryName()),Collectors.counting()));
+			                                                   
+    System.out.println("total players by country are" + totalPlayers);
+	}
+	
+	static void getTotalRunsByCountry() {
+		
+		
+		Map<String,Integer> totalRuns = players.stream().collect(
+				Collectors.groupingBy((p -> p.getCountry().getCountryName()),Collectors.summingInt(Player :: getRuns)));
+		
+		System.out.println("total runs by country are" + totalRuns);
+	}
+	
+	static void getMaxScoreByCountry(){
+		
+		Map<String,Optional<Player>> maxScore = players.stream()
+				                                        .collect(Collectors.groupingBy(
+				                                        		(p -> p.getCountry().getCountryName()),Collectors.maxBy(Comparator.comparing(Player :: getRuns))));
+		
+		System.out.println("maxScore by country " + maxScore);		
+		
+	}
+	
+	static void getPlayerNamesStringByCountry() {
+		Map<String,String> playerStr = players.stream().collect(Collectors.groupingBy(p->p.getCountry().getCountryName(),Collectors.mapping(Player :: getPlayerName, Collectors.joining(","))));
+	
+		System.out.println("Player names as String are " + playerStr);
+	
+	}
+	
+	public static void main(String[] args) {
+		getPlayersByCountry();
+		getPlayerNamesByCountry();
+		getTotalPlayersByCountry();
+		getTotalRunsByCountry();
+		getMaxScoreByCountry();
+		getPlayerNamesStringByCountry();
+		}
 }
